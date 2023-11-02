ef02b6d58c31d0dbd0a211c84aa778d34fa87d4e
Mudasir Wani <mudasirwani88@yahoo.in>
2023-11-02 22:26:29 +0530

    Add files via upload

---
+++ Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext )
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            //var regions = new List<Region>
+            //{
+            //new Region
+            //{
+            //Id= Guid.NewGuid(),
+            //Name = "Auckland Region",
+            //Code = "AKL",
+            //RegionImageUrl = ""
+            //},
+            //new Region
+            //{
+            //Id= Guid.NewGuid(),
+            //Name = "Wellington Region",
+            //Code = "WLG",
+            //RegionImageUrl = ""
+            //}
+            //};
+            var regions = dbContext.Regions.ToList();
+            return Ok(regions);
+        }
+    }
+}
---
+++ Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        //Navigation Properties
+        public Region Region { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+}
